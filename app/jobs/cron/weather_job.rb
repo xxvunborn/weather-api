dcc0991908ba42d6c964ac6ecf862f86f8d46566
Chris <christian.munozp@mail.udp.cl>
2018-09-10 11:12:44 -0300

    comment older gem

--- app/jobs/cron/weather_job.rb
+++ app/jobs/cron/weather_job.rb
@@ -1,33 +1,33 @@
-require 'sidekiq-scheduler'
-require 'forecast_io'
-require 'dotenv/load'
+#require 'sidekiq-scheduler'
+#require 'forecast_io'
+#require 'dotenv/load'
 
-class WeatherJob
-  include Sidekiq::Worker
+#class WeatherJob
+  #include Sidekiq::Worker
 
-  def perform
-    cities_coordinates.each do |city|
-      forecast = ForecastIO.forecast(city[1]['longitude'], city[1]['latitude'])
-      redis.set(city[0] + '_weather', forecast['currently'].to_json)
-    end
-  end
+  #def perform
+    #cities_coordinates.each do |city|
+      #forecast = ForecastIO.forecast(city[1]['longitude'], city[1]['latitude'])
+      #redis.set(city[0] + '_weather', forecast['currently'].to_json)
+    #end
+  #end
 
-  private
+  #private
 
-  def cities_coordinates
-    cities_name = %w[Santiago Zurich Auckland Sydney Londres Georgia]
-    cities = {}
-    cities_name.each do |city|
-      cities[city] = JSON.parse(redis.get(city))
-    end
-    cities
-  end
+  #def cities_coordinates
+    #cities_name = %w[Santiago Zurich Auckland Sydney Londres Georgia]
+    #cities = {}
+    #cities_name.each do |city|
+      #cities[city] = JSON.parse(redis.get(city))
+    #end
+    #cities
+  #end
 
-  def redis
-    @redis = Redis.new
-  end
+  #def redis
+    #@redis = Redis.new
+  #end
 
-  ForecastIO.configure do |configuration|
-    configuration.api_key = ENV['FORECAST_KEY']
-  end
-end
+  #ForecastIO.configure do |configuration|
+    #configuration.api_key = ENV['FORECAST_KEY']
+  #end
+#end
