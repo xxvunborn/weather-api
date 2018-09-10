e66f421fbfcd648a405836c88be86e71745d0c70
Chris <christian.munozp@mail.udp.cl>
2018-09-10 11:51:24 -0300

    sidekiq conf

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,20 @@
+require 'sidekiq'
+
+Sidekiq.configure_client do |config|
+  config.redis = { :size => 1 }
+end
+
+Sidekiq.configure_server do |config|
+  # The config.redis is calculated by the
+  # concurrency value so you do not need to
+  # specify this. For this demo I do
+  # show it to understand the numbers
+  config.redis = { :size => 9 }
+end
+
 schedule_file = "config/schedule.yml"
 
 if File.exist?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
+
